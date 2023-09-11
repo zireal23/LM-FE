e34943473403f523dec1ec21a6a657f298a32a2f
SharmaRiya01 <61745660+SharmaRiya01@users.noreply.github.com>
2023-09-11 22:08:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AddEmployee from"./components/AddEmployee";
+
+function App() {
+  return (
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<AddEmployee/>}></Route>
+  </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
