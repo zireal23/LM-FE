62e9b19b86f82d78c86b4572273b245af16acda2
zireal23 <skasif232000@gmail.com>
2023-09-12 21:41:31 +0530

    added the user dashboard

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './UserDashboard.css';
 
 function UserDashboard() {
   return (
-    <div>UserDashboard</div>
-  )
+    <div className="user-dashboard">
+      <h1>User Dashboard</h1>
+      <div className="button-container">
+        <button className="dashboard-button">View Loans</button>
+        <button className="dashboard-button">Apply for Loan</button>
+        <button className="dashboard-button">View Items Purchased</button>
+      </div>
+    </div>
+  );
 }
 
-export default UserDashboard
+export default UserDashboard;
