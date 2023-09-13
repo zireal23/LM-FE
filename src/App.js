0cf732d7351195b6a298d3cca0fb2732aaf86f57
zireal23 <skasif232000@gmail.com>
2023-09-13 11:05:51 +0530

    added functionality to buttons

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddEmployee from"./components/AddEmployee";
 import LoginPage from './components/login';
 import ViewLoans from './components/ViewLoan';
-
+import UserDashboard from './components/UserDashboard';
+import Loanapply from './components/Loanapply';
+import Header from "./components/Header";
 function App() {
   return (
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<AddEmployee/>}></Route>
-    <Route path="/login" element={<LoginPage/>}></Route>
-    <Route path="/fetch" element={<ViewLoans/>}></Route>
-    
-
-  </Routes>
-  </BrowserRouter>
-  );
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AddEmployee/>}></Route>
+      <Route path="/userdashboard" element={<UserDashboard/>}/>
+      <Route path="/login" element={<LoginPage/>}></Route>
+      <Route path="/fetch" element={<ViewLoans/>}></Route>
+      <Route path="/applyLoan" element={<Loanapply/>}></Route>
+  
+    </Routes>
+    </BrowserRouter>
+    </>
+    );
+ 
 }
 
 export default App;
