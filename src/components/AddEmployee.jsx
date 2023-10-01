ff9475023e1034eaceedef27cce39853cff2b117
zireal23 <skasif232000@gmail.com>
2023-10-01 13:09:59 +0530

    fixed responsivess of login and signup page, fixed navbar styling

--- src/components/AddEmployee.jsx
+++ src/components/AddEmployee.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-//import { Form, Button, Container, Alert } from 'react-bootstrap';
-import { FaUser, FaLock, FaIdCard, FaDesktop, FaDeskpro, FaTransgender, FaGenderless, FaMale, FaCalendar } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { FaUser, FaLock, FaIdCard, FaDesktop, FaDeskpro, FaMale, FaCalendar } from 'react-icons/fa';
 
 const AddEmployee = () => {
   const navigate = useNavigate();
-  const baseURL = "http://localhost:7000/saveEmployee";
+  const baseURL = 'http://localhost:7000/saveEmployee';
   const [employeeId, setId] = useState('');
   const [employeeName, setEmployeeName] = useState('');
   const [department, setDepartment] = useState('');
@@ -15,10 +14,57 @@ const AddEmployee = () => {
   const [dateofbirth, setdateofbirth] = useState('');
   const [dateofjoining, setDateofjoining] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
 
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = {};
+
+    if (!employeeId) {
+      newErrors.employeeId = 'Employee ID is required';
+      isValid = false;
+    }
+
+    if(employeeName&&/\d/.test(employeeName)){
+      isValid = false;
+      newErrors.employeeName = "EmployeeName cannot have number or special characters"
+  }
+
+    if(department&&/\d/.test(department)){
+        isValid = false;
+        newErrors.department = "Department cannot have number or special characters"
+    }
+
+    if(designation&&/\d/.test(designation)){
+      isValid = false;
+      newErrors.designation = "Designation cannot have number or special characters"
+  }
+
+  if((gender&&/\d/.test(gender))||gender.length>1){
+    isValid = false;
+    newErrors.gender = "Gender cannot have number or special characters and should be of one character only"
+}
+
+    if (!dateofbirth) {
+      newErrors.dateofbirth = 'Date of Birth is required';
+      isValid = false;
+    }
+
+    if (!dateofjoining) {
+      newErrors.dateofjoining = 'Date of Joining is required';
+      isValid = false;
+    }
+
+    if (!password) {
+      newErrors.password = 'Password is required';
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
 
   const employeeIdChangeHandler = (event) => {
-    //alert(event.target.value);
     setId(event.target.value);
   };
 
@@ -29,51 +75,54 @@ const AddEmployee = () => {
   const departmentChangeHandler = (event) => {
     setDepartment(event.target.value);
   };
+
   const designationChangeHandler = (event) => {
     setDesignation(event.target.value);
   };
+
   const genderChangeHandler = (event) => {
     setGender(event.target.value);
   };
+
   const dateofbirthChangeHandler = (event) => {
     setdateofbirth(event.target.value);
   };
+
   const dateofjoiningChangeHandler = (event) => {
     setDateofjoining(event.target.value);
   };
+
   const passwordChangeHandler = (event) => {
     setPassword(event.target.value);
   };
 
-
-
   const submitActionHandler = (event) => {
     event.preventDefault();
-    axios
-      .post(baseURL, {
-        employeeId: employeeId,
-        employeeName: employeeName,
-        department: department,
-        designation: designation,
-        gender: gender,
-        dateofbirth: dateofbirth,
-        dateofjoining: dateofjoining,
-        password: password
-      })
-      .then((response) => {
-        // alert(response.data.employeeName);
-        console.log(response.data);
-        alert("Employee " + employeeName + " added!");
-        navigate("/viewemployees");
 
-      }).catch(error => {
-        alert("error===" + error);
-      });
-
+    if (validateForm()) {
+      axios
+        .post(baseURL, {
+          employeeId: employeeId,
+          employeeName: employeeName,
+          department: department,
+          designation: designation,
+          gender: gender,
+          dateofbirth: dateofbirth,
+          dateofjoining: dateofjoining,
+          password: password,
+        })
+        .then((response) => {
+          console.log(response.data);
+          alert('Employee ' + employeeName + ' added!');
+          navigate('/viewemployees');
+        })
+        .catch((error) => {
+          alert('error===' + error);
+        });
+    } 
   };
 
   const cancelHandler = () => {
-    //reset the values of input fields
     setId('');
     setEmployeeName('');
     setDesignation('');
@@ -82,75 +131,105 @@ const AddEmployee = () => {
     setdateofbirth('');
     setDateofjoining('');
     setPassword('');
-    // navigate("/read");
+  };
 
-  }
   return (
-
     <div className='addEmployee'>
-
       <form onSubmit={submitActionHandler}>
-
-        <h2 className="title">Add Employee</h2>
+        <h2 className='title'>Add Employee</h2>
 
         <div className='input-field'>
-          <i className="fas fa-id-card-o">
-            <FaIdCard style={{ color: "#121212" }} />
+          <i className='fas fa-id-card-o'>
+            <FaIdCard style={{ color: '#121212' }} />
           </i>
-          <input type="text" value={employeeId} onChange={employeeIdChangeHandler} placeholder="Employee ID" required />
+          <input type='text' value={employeeId} onChange={employeeIdChangeHandler} placeholder='Employee ID' required />
+          <span className='error'>{errors.employeeId}</span>
         </div>
 
         <div className='input-field'>
-          <i className="fas fa-user">
-            <FaUser style={{ color: "#121212" }} />
+          <i className='fas fa-user'>
+            <FaUser style={{ color: '#121212' }} />
           </i>
-          <input type="text" value={employeeName} onChange={employeeNameChangeHandler} placeholder="Employee Name" required /></div>
+          <input
+            type='text'
+            value={employeeName}
+            onChange={employeeNameChangeHandler}
+            placeholder='Employee Name'
+            required
+          />
+          <span className='error'>{errors.employeeName}</span>
+        </div>
 
         <div className='input-field'>
-          <i className="fas fa-desktop">
-            <FaDesktop style={{ color: "#121212" }} />
+          <i className='fas fa-desktop'>
+            <FaDesktop style={{ color: '#121212' }} />
           </i>
-          <input type="text" value={department} onChange={departmentChangeHandler} placeholder="Department" required /></div>
-        {/* <select>
-          <option value={department} onChange={departmentChangeHandler}>Option1</option>
-          <option value={department} onChange={departmentChangeHandler}>Option2</option>
-          <option value={department} onChange={departmentChangeHandler}>Option3</option>
-          <option value={department} onChange={departmentChangeHandler}>Option4</option>
-        </select> */}
+          <input
+            type='text'
+            value={department}
+            onChange={departmentChangeHandler}
+            placeholder='Department'
+            required
+          />
+          <span className='error'>{errors.department}</span>
+        </div>
 
         <div className='input-field'>
-          <i className="fas fa-deskpro">
-            <FaDeskpro style={{ color: "#121212" }} />
+          <i className='fas fa-deskpro'>
+            <FaDeskpro style={{ color: '#121212' }} />
           </i>
-          <input type="text" value={designation} onChange={designationChangeHandler} placeholder="Designation" required /></div>
-        <div className='input-field'>
-          <i className="fas fa-male">
-            <FaMale style={{ color: "#121212" }} />
-          </i>
-          <input type="text" value={gender} onChange={genderChangeHandler} placeholder="Gender" required /></div>
-        <div className='input-field'>
-          <i className="fas fa-calendar">
-            <FaCalendar style={{ color: "#121212" }} />
-          </i>
-          <input type="date" value={dateofbirth} onChange={dateofbirthChangeHandler}  placeholder="Date of Birth" required /></div>
-        <div className='input-field'>
-          <i className="fas fa-calendar">
-            <FaCalendar style={{ color: "#121212" }} />
-          </i>
-          <input type="date" value={dateofjoining} onChange={dateofjoiningChangeHandler} placeholder="Date of Joining" required /></div>
-        <div className='input-field'>
-          <i className="fas fa-lock">
-            <FaLock style={{ color: "#121212" }} />
-          </i>
-          <input type="password" value={password} onChange={passwordChangeHandler} placeholder="Password" required /></div>
+          <input
+            type='text'
+            value={designation}
+            onChange={designationChangeHandler}
+            placeholder='Designation'
+            required
+          />
+          <span className='error'>{errors.designation}</span>
+        </div>
 
-        <button type='submit' className="btn solid">Add Employee</button>
+        <div className='input-field'>
+          <i className='fas fa-male'>
+            <FaMale style={{ color: '#121212' }} />
+          </i>
+          <input type='text' value={gender} onChange={genderChangeHandler} placeholder='Gender' required />
+          <span className='error'>{errors.gender}</span>
+        </div>
+
+        <div className='input-field'>
+          <i className='fas fa-calendar'>
+            <FaCalendar style={{ color: '#121212' }} />
+          </i>
+          <input type='date' value={dateofbirth} onChange={dateofbirthChangeHandler} required />
+          <span className='error'>{errors.dateofbirth}</span>
+        </div>
+
+        <div className='input-field'>
+          <i className='fas fa-calendar'>
+            <FaCalendar style={{ color: '#121212' }} />
+          </i>
+          <input type='date' value={dateofjoining} onChange={dateofjoiningChangeHandler} required />
+          <span className='error'>{errors.dateofjoining}</span>
+        </div>
+
+        <div className='input-field'>
+          <i className='fas fa-lock'>
+            <FaLock style={{ color: '#121212' }} />
+          </i>
+          <input type='password' value={password} onChange={passwordChangeHandler} placeholder='Password' required />
+          <span className='error'>{errors.password}</span>
+        </div>
+
+        <button type='submit' className='btn solid'>
+          Add Employee
+        </button>
         &nbsp;&nbsp;&nbsp;
-        <button type='reset' className="btn solid" onClick={() => cancelHandler()}>Cancel</button>
+        <button type='reset' className='btn solid' onClick={() => cancelHandler()}>
+          Cancel
+        </button>
       </form>
-
     </div>
-
   );
-}
+};
+
 export default AddEmployee;
