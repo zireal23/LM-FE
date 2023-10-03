6847846f146710c285ea64dc8e7798ed964ad081
this-is-aishwarya <akkusush@gmail.com>
2023-10-03 12:28:03 +0530

    Delete and loan apply toast added

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -3,8 +3,13 @@ import {useNavigate} from "react-router-dom";
 import { useParams } from 'react-router-dom'; 
 import axios from "axios";
 //import { Form, Button, Container, Alert } from 'react-bootstrap';
+import { ToastContainer, Toast } from 'react-bootstrap';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Delete = () => {
+  const [showToastSuccess, setShowToastSuccess] = useState(false)
+  const [showToastFail, setShowToastFail] = useState(false)
+  const [toastMessage, setToastMessage] = useState('')
   const navigate = useNavigate();
   const baseURL = `http://localhost:7000/deleteempbyid`;
   const  {employeeId}  = useParams();
@@ -18,11 +23,15 @@ const Delete = () => {
       .delete(baseURL, {params: { employeeId: employeeId}})
       .then((response) => {
        // alert(response.data.employeeName);
-       console.log(response.data);
-        navigate("/viewemployees");
+       setShowToastSuccess(true)
+          setToastMessage("User deleted successfully")
+          setTimeout(() => {
+            navigate('/viewemployees');
+          }, 3000);
         
       }).catch(error => {
-        alert("error==="+error);
+        setShowToastFail(true)
+          setToastMessage("User unable to delete");
       });
 
   };
@@ -44,6 +53,20 @@ const Delete = () => {
         &nbsp;&nbsp;&nbsp;
         <button type='reset' className="loginButton transparent" onClick={()=>cancelHandler()}>No</button> 
       </form>
+
+      <ToastContainer style={{ top: "10px", right: "10px", position:"fixed" }}>
+
+         <Toast show={showToastFail} onClose={() => setShowToastFail(false)} delay={3000} autohide bg="danger" style={{color:"#fff", backgroundColor:"#CA0800", padding:"2em"}}>
+
+           <Toast.Body>{toastMessage}</Toast.Body>
+       </Toast>
+      </ToastContainer>
+      <ToastContainer style={{ top: "10px", right: "10px", position:"fixed" }} >
+
+        <Toast show={showToastSuccess} onClose={() => setShowToastSuccess(false)} delay={3000} autohide bg='success' style={{color:"#fff", backgroundColor:"#28A745", padding:"2em"}}>
+          <Toast.Body>{toastMessage}</Toast.Body>
+        </Toast>
+      </ToastContainer>
 
       </div>
     
